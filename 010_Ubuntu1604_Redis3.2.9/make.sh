e5ed5d737275fc7e6ba334444b60cc9eb7fb1ef8
hexin0614@gmail.com <hexin0614@gmail.com>
2017-06-29 22:46:30 +0800

    Remove redis install file, use download script instead

--- 010_Ubuntu1604_Redis3.2.9/make.sh
+++ 010_Ubuntu1604_Redis3.2.9/make.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# download redis install package and verify md5
+wget http://download.redis.io/releases/redis-3.2.9.tar.gz
+echo "3d0a35985b4414c9376b6bcda0ef72ae *./redis-3.2.9.tar.gz" | md5sum -c
+
+# IF YOU CAN NOT DOWNLOAD REDIS INSTALL FILE OR YOU BEHIND A FIREWALL,
+# PLEASE CONSIDER FOLLOWING SOLUTION
 # copy required file to working folder
-cp ../090_Share/redis-3.2.9.tar.gz .
+# cp ../090_Share/redis-3.2.9.tar.gz .
+
 cp ../090_Share/sources.aliyun.list ./sources.list
 
 # change property
