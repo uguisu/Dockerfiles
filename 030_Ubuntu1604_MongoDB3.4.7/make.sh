5bcee7324cb5799f154973f847ec58717f7709c7
hexin0614 <hexin0614@hotmail.com>
2017-09-04 21:51:52 +0800

    invite mongodb

---
+++ 030_Ubuntu1604_MongoDB3.4.7/make.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Usage: make.sh <current user name> <config file name> <server IP>
+
+# download install package and verify md5
+if [ ! -e "./mongodb-linux-x86_64-ubuntu1604-3.4.7.tgz" ]
+then
+    wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1604-3.4.7.tgz
+#    echo "0086ecf960757f28ca7f48ed70f605159f87c626 *./logstash-5.4.3.tar.gz" | sha1sum -c
+fi
+
+# IF YOU CAN NOT DOWNLOAD INSTALL FILE OR YOU BEHIND A FIREWALL,
+# PLEASE CONSIDER FOLLOWING SOLUTION
+# copy required file to working folder
+# cp ../090_Share/mongodb-linux-x86_64-ubuntu1604-3.4.7.tgz .
+
+cp ../090_Share/sources.aliyun.list ./sources.list
+
+# change property
+chown -R $1:$1 ../../
+
+# build
+docker build -t='uguisu/ubuntu1604/mongodb347:latest' .
